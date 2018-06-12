e5be55c57bbf631bf46362c80f654c2e46660717
devan.xu <devan.xu@foxmail.com>
2018-06-12 21:58:09 +0800

    use ListView instead of List for CustomerList

--- src/containers/customer.js
+++ src/containers/customer.js
@@ -1,87 +1,188 @@
 import React from "react";
+import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
 import { setName } from '../actions/customerAction'
 
-import { List } from 'antd-mobile';
+/* eslint no-dupe-keys: 0, no-mixed-operators: 0 */
+import { ListView } from 'antd-mobile';
 
-const Item = List.Item;
-const Brief = Item.Brief;
+function MyBody(props) {
+    return (
+        <div className="am-list-body my-body">
+            <span style={{ display: 'none' }}>you can custom body wrap element</span>
+            {props.children}
+        </div>
+    );
+}
+
+const data = [
+    {
+        img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
+        title: 'Meet hotel',
+        des: '不是所有的兼职汪都需要风吹日晒',
+    },
+    {
+        img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
+        title: 'McDonald\'s invites you',
+        des: '不是所有的兼职汪都需要风吹日晒',
+    },
+    {
+        img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
+        title: 'Eat the week',
+        des: '不是所有的兼职汪都需要风吹日晒',
+    },
+];
+const NUM_SECTIONS = 5;
+const NUM_ROWS_PER_SECTION = 5;
+let pageIndex = 0;
+
+const dataBlobs = {};
+let sectionIDs = [];
+let rowIDs = [];
+function genData(pIndex = 0) {
+    for (let i = 0; i < NUM_SECTIONS; i++) {
+        const ii = (pIndex * NUM_SECTIONS) + i;
+        const sectionName = `Section ${ii}`;
+        sectionIDs.push(sectionName);
+        dataBlobs[sectionName] = sectionName;
+        rowIDs[ii] = [];
+
+        for (let jj = 0; jj < NUM_ROWS_PER_SECTION; jj++) {
+            const rowName = `S${ii}, R${jj}`;
+            rowIDs[ii].push(rowName);
+            dataBlobs[rowName] = rowName;
+        }
+    }
+    sectionIDs = [...sectionIDs];
+    rowIDs = [...rowIDs];
+}
 
 class CustomerList extends React.Component {
+    constructor(props) {
+        super(props);
+        const getSectionData = (dataBlob, sectionID) => dataBlob[sectionID];
+        const getRowData = (dataBlob, sectionID, rowID) => dataBlob[rowID];
+
+        const dataSource = new ListView.DataSource({
+            getRowData,
+            getSectionHeaderData: getSectionData,
+            rowHasChanged: (row1, row2) => row1 !== row2,
+            sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+        });
+
+        this.state = {
+            dataSource,
+            isLoading: true,
+            height: document.documentElement.clientHeight * 3 / 4,
+        };
+    }
+
+    componentDidMount() {
+        // you can scroll to the specified position
+        // setTimeout(() => this.lv.scrollTo(0, 120), 800);
+
+        const hei = document.documentElement.clientHeight - ReactDOM.findDOMNode(this.lv).parentNode.offsetTop;
+        // simulate initial Ajax
+        setTimeout(() => {
+            genData();
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
+                isLoading: false,
+                height: hei,
+            });
+        }, 600);
+    }
+
+    // If you use redux, the data maybe at props, you need use `componentWillReceiveProps`
+    // componentWillReceiveProps(nextProps) {
+    //   if (nextProps.dataSource !== this.props.dataSource) {
+    //     this.setState({
+    //       dataSource: this.state.dataSource.cloneWithRowsAndSections(nextProps.dataSource),
+    //     });
+    //   }
+    // }
+
+    onEndReached = (event) => {
+        // load new data
+        // hasMore: from backend data, indicates whether it is the last page, here is false
+        if (this.state.isLoading && !this.state.hasMore) {
+            return;
+        }
+        console.log('reach end', event);
+        this.setState({ isLoading: true });
+        setTimeout(() => {
+            genData(++pageIndex);
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRowsAndSections(dataBlobs, sectionIDs, rowIDs),
+                isLoading: false,
+            });
+        }, 1000);
+    }
+
     render() {
-        return (<div>
-            <List renderHeader={() => 'Basic Style'} className="my-list">
-                <Item extra={'extra content'}>Title</Item>
-            </List>
-            <List renderHeader={() => 'Subtitle'} className="my-list">
-                <Item arrow="horizontal" multipleLine onClick={() => { }}>
-                    Title <Brief>subtitle</Brief>
-                </Item>
-                <Item
-                    arrow="horizontal"
-                    multipleLine
-                    onClick={() => { }}
-                    platform="android"
-                >
-                    ListItem （Android）<Brief>There may have water ripple effect of <br /> material if you set the click event.</Brief>
-                </Item>
-                <Item
-                    arrow="horizontal"
-                    thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
-                    multipleLine
-                    onClick={() => { }}
-                >
-                    Title <Brief>subtitle</Brief>
-                </Item>
-            </List>
-            <List renderHeader={() => 'Customized Right Side（Empty Content / Text / Image）'} className="my-list">
-                <Item>Title</Item>
-                <Item arrow="horizontal" onClick={() => { }}>Title</Item>
-                <Item extra="extra content" arrow="horizontal" onClick={() => { }}>Title</Item>
-                <Item extra="10:30" align="top" thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png" multipleLine>
-                    Title <Brief>subtitle</Brief>
-                </Item>
-            </List>
-            <List renderHeader={() => 'Align Vertical Center'} className="my-list">
-                <Item multipleLine extra="extra content">
-                    Title <Brief>subtitle</Brief>
-                </Item>
-            </List>
-            <List renderHeader={() => 'Icon in the left'}>
-                <Item
-                    thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
-                    arrow="horizontal"
-                    onClick={() => { }}
-                >My wallet</Item>
-                <Item
-                    thumb="https://zos.alipayobjects.com/rmsportal/UmbJMbWOejVOpxe.png"
-                    onClick={() => { }}
-                    arrow="horizontal"
-                >
-                    My Cost Ratio
-        </Item>
-            </List>
-            <List renderHeader={() => 'Text Wrapping'} className="my-list">
-                <Item data-seed="logId">Single line，long text will be hidden with ellipsis；</Item>
-                <Item wrap>Multiple line，long text will wrap；Long Text Long Text Long Text Long Text Long Text Long Text</Item>
-                <Item extra="extra content" multipleLine align="top" wrap>
-                    Multiple line and long text will wrap. Long Text Long Text Long Text
-        </Item>
-                <Item extra="no arrow" arrow="empty" className="spe" wrap>
-                    In rare cases, the text of right side will wrap in the single line with long text. long text long text long text
-        </Item>
-            </List>
-            <List renderHeader={() => 'Other'} className="my-list">
-                <Item disabled={this.props.customer.disabled} extra="" onClick={() => { console.log('click', this.props.customer.disabled); this.setState({ disabled: true }); }}>Click to disable</Item>
-                <Item>
-                    <select defaultValue="1">
-                        <option value="1">Html select element</option>
-                        <option value="2" disabled>Unable to select</option>
-                        <option value="3">option 3</option>
-                    </select>
-                </Item>
-            </List>
-        </div>);
+        const separator = (sectionID, rowID) => (
+            <div
+                key={`${sectionID}-${rowID}`}
+                style={{
+                    backgroundColor: '#F5F5F9',
+                    height: 8,
+                    borderTop: '1px solid #ECECED',
+                    borderBottom: '1px solid #ECECED',
+                }}
+            />
+        );
+        let index = data.length - 1;
+        const row = (rowData, sectionID, rowID) => {
+            if (index < 0) {
+                index = data.length - 1;
+            }
+            const obj = data[index--];
+            return (
+                <div key={rowID} style={{ padding: '0 15px' }}>
+                    <div
+                        style={{
+                            lineHeight: '50px',
+                            color: '#888',
+                            fontSize: 18,
+                            borderBottom: '1px solid #F6F6F6',
+                        }}
+                    >{obj.title}</div>
+                    <div style={{ display: '-webkit-box', display: 'flex', padding: '15px 0' }}>
+                        <img style={{ height: '64px', marginRight: '15px' }} src={obj.img} alt="" />
+                        <div style={{ lineHeight: 1 }}>
+                            <div style={{ marginBottom: '8px', fontWeight: 'bold' }}>{obj.des}</div>
+                            <div><span style={{ fontSize: '30px', color: '#FF6E27' }}>35</span>¥ {rowID}</div>
+                        </div>
+                    </div>
+                </div>
+            );
+        };
+
+        return (
+            <ListView
+                ref={el => this.lv = el}
+                dataSource={this.state.dataSource}
+                renderHeader={() => <span>header</span>}
+                renderFooter={() => (<div style={{ padding: 30, textAlign: 'center' }}>
+                    {this.state.isLoading ? 'Loading...' : 'Loaded'}
+                </div>)}
+                renderSectionHeader={sectionData => (
+                    <div>{`Task ${sectionData.split(' ')[1]}`}</div>
+                )}
+                renderBodyComponent={() => <MyBody />}
+                renderRow={row}
+                renderSeparator={separator}
+                style={{
+                    height: this.state.height,
+                    overflow: 'auto',
+                }}
+                pageSize={4}
+                onScroll={() => { console.log('scroll'); }}
+                scrollRenderAheadDistance={500}
+                onEndReached={this.onEndReached}
+                onEndReachedThreshold={10}
+            />
+        );
     }
 }
 
