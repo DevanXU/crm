e0a493a41413c89a85e16237a6feaa9cf48b77f5
devan.xu <devan.xu@foxmail.com>
2018-06-23 18:56:27 +0800

    add Navigator for CustomerInfo

--- src/components/customerinfo.js
+++ src/components/customerinfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Tabs, WhiteSpace, Badge } from 'antd-mobile';
+import Navigator from './navigator'
 
 const tabs = [
     { title: <Badge text={'3'}>First Tab</Badge> },
@@ -17,40 +18,28 @@ const tabs2 = [
 class CustomerInfo extends React.Component {
     render() {
         return (
-
             <div>
-                <Tabs tabs={tabs}
-                    initialPage={1}
-                    onChange={(tab, index) => { console.log('onChange', index, tab); }}
-                    onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
-                >
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-                        Content of first tab
+                <div>
+                    <Navigator />
+                </div>
+                <div>
+                    <Tabs tabs={tabs}
+                        initialPage={1}
+                        onChange={(tab, index) => { console.log('onChange', index, tab); }}
+                        onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+                    >
+                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+                            Content of first tab
                     </div>
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-                        Content of second tab
+                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+                            Content of second tab
                     </div>
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-                        Content of third tab
+                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+                            Content of third tab
                     </div>
-                </Tabs>
-                <WhiteSpace />
-                <Tabs tabs={tabs2}
-                    initialPage={1}
-                    tabBarPosition="bottom"
-                    renderTab={tab => <span>{tab.title}</span>}
-                >
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-                        Content of first tab
-                    </div>
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-                        Content of second tab
-                    </div>
-                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
-                        Content of third tab
-                    </div>
-                </Tabs>
-                <WhiteSpace />
+                    </Tabs>
+                    <WhiteSpace />
+                </div>
             </div>
         )
     }
