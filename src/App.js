c728bfdd90ab77e5bf649ac186fca265bef68e33
devan.xu <devan.xu@foxmail.com>
2018-06-25 10:40:55 +0800

    Fix "Switch" typo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import store from './store'
 import Home from './components/home'
 import CusotmerList from './containers/customer'
@@ -14,12 +14,12 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Provider store={store}>
-          <switch className="App">
+          <Switch className="App">
             <Route exact path='/' component={Home} />
             <Route exact path='/sale' component={Sale} />
             <Route exact path='/customer' component={CusotmerList} />
             <Route path={`/customer/:customerId`} component={CustomerInfo} />
-          </switch>
+          </Switch>
         </Provider>
       </BrowserRouter >
     );
